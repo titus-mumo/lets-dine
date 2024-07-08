04f2f97d96e68979724b49ea2482d256437aef58
Titus Kennedy <tituskennedy74@gmail.com>
2024-07-08 08:58:44 +0300

    just code

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -8,6 +8,7 @@ import { useNavigate } from 'react-router-dom';
 
 import { ToastMessage } from '../utils';
 import LoadingSpinner from './LandingPage';
+import home2 from '../assets/home2.jpg';
 
 
 //TODO: spacing and just styling
@@ -27,6 +28,12 @@ export const Login = () => {
         setTimeout(() => {
             setLoading(false)
         }, 1500)
+        // const userRole = localStorage.getItem("role")
+        // if(userRole){
+        //     console.log("hello")
+        //     ToastMessage("info", "You are already logged in!")
+        //     navigate('/home')
+        // }
     }, [])
 
     
@@ -66,49 +73,107 @@ export const Login = () => {
         return;
     };
   return (
-        <section className="h-lvh md:h-full w-full bg-gray-900">
-            {
-                loading? <LoadingSpinner />:
-                <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                <a href="/" className="flex text-white items-center mb-3 text-lg md:text-xl  font-semibold">
-                    EthnicEats   
+    <section className="w-full bg-white min-h-screen flex items-center justify-center relative">
+        {loading ? (
+            <LoadingSpinner />
+        ) : (
+            <div className="flex flex-col items-center justify-center w-full">
+                <a href="/" className="flex text-gray-900 items-center mb-3 text-2xl md:text-xl font-semibold z-10">
+                    EthnicEats
                 </a>
-                <div className="w-300px bg-white rounded-lg shadow dark:border sm:max-w-xs md:max-w-sm xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-                    <div className="p-3 space-y-2 md:space-y-4 sm:p-4">
-                        <h1 className="text-sm font-bold leading-tight tracking-tight text-gray-900 md:text-lg dark:text-white">
-                            Sign in to your account
-                        </h1>
-                        <form className="space-y-3 md:space-y-5" onSubmit={(e) => handleLogin(e)}>
-                            <div>
-                                <label htmlFor="username" className="block mb-1 text-xs md:text-sm font-medium text-gray-900 dark:text-white">Your Username</label>
-                                <input onChange={(e) => setUsername(e.target.value)} value={username} type="text" name="username" id="username" className="bg-gray-50 border border-gray-300 text-gray-900 text-xs md:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Username" required />
-                            </div>
-                            <div>
-                                <label htmlFor="password" className="block mb-1 text-xs md:text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                                <input onChange={(e) => setPassword(e.target.value)} value={password} type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-xs md:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
-                            </div>
-                            <div className="flex items-center justify-between">
-                                <div className="flex items-start">
-                                    <div className="flex items-center h-5">
-                                        <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800" />
-                                    </div>
-                                    <div className="ml-3 text-sm">
-                                        <label htmlFor="remember" className="text-gray-500 dark:text-gray-300">Remember me</label>
-                                    </div>
+                <div className='flex flex-col md:flex-row w-3/4 relative'>
+                    <div className="w-full md:w-5/6 lg:w-3/4 bg-white shadow dark:border dark:bg-gray-800 dark:border-gray-700 z-10 relative">
+                        <div className="p-3 space-y-2 md:space-y-4 sm:p-4">
+                            <h1 className="text-sm font-bold leading-tight tracking-tight text-gray-900 md:text-lg dark:text-white">
+                                Sign in to your account
+                            </h1>
+                            <form className="space-y-3 md:space-y-5" onSubmit={(e) => handleLogin(e)}>
+                                <div>
+                                    <label htmlFor="username" className="block mb-1 text-xs md:text-sm font-medium text-gray-900 dark:text-white">
+                                        Your Username
+                                    </label>
+                                    <input 
+                                        onChange={(e) => setUsername(e.target.value)} 
+                                        value={username} 
+                                        type="text" 
+                                        name="username" 
+                                        id="username" 
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 text-xs md:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                                        placeholder="Username" 
+                                        required 
+                                    />
                                 </div>
-                                <a href="/forgot-password" className="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">Forgot password?</a>
-                            </div>
-                            <button type="submit" className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-2 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-500 dark:hover:bg-blue-600 dark:focus:ring-blue-800">Sign in</button>
-                            <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                                Don’t have an account yet? <a href="/register" className="font-medium text-blue-600 hover:underline dark:text-blue-500">Sign up</a>
-                            </p>
-                            <a href="/" className="block mt-3 text-center text-sm text-gray-500 dark:text-gray-400">Go back to homepage</a>
-                        </form>
+                                <div>
+                                    <label htmlFor="password" className="block mb-1 text-xs md:text-sm font-medium text-gray-900 dark:text-white">
+                                        Password
+                                    </label>
+                                    <input 
+                                        onChange={(e) => setPassword(e.target.value)} 
+                                        value={password} 
+                                        type="password" 
+                                        name="password" 
+                                        id="password" 
+                                        placeholder="••••••••" 
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 text-xs md:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                                        required 
+                                    />
+                                </div>
+                                <div className="flex items-center justify-between">
+                                    <div className="flex items-start">
+                                        <div className="flex items-center h-5">
+                                            <input 
+                                                id="remember" 
+                                                aria-describedby="remember" 
+                                                type="checkbox" 
+                                                className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800" 
+                                            />
+                                        </div>
+                                        <div className="ml-3 text-sm">
+                                            <label htmlFor="remember" className="text-gray-500 dark:text-gray-300">
+                                                Remember me
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <a href="/forgot-password" className="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">
+                                        Forgot password?
+                                    </a>
+                                </div>
+                                <button 
+                                    type="submit" 
+                                    className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-2 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-500 dark:hover:bg-blue-600 dark:focus:ring-blue-800"
+                                >
+                                    Sign in
+                                </button>
+                                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                                    Don’t have an account yet? 
+                                    <a href="/register" className="ml-2 font-medium text-blue-600 hover:underline dark:text-blue-500">
+                                        Sign up
+                                    </a>
+                                </p>
+                                <a href="/" className="block mt-3 text-center text-sm text-gray-500 dark:text-gray-400">
+                                    Go back to homepage
+                                </a>
+                            </form>
+                        </div>
+                    </div>
+                    <div className='hidden md:block sm:w-1/2 absolute md:relative top-0 right-0 bottom-0 left-auto'>
+                        <img 
+                            src={home2} 
+                            alt="home" 
+                            className="w-full h-full object-cover object-center"
+                        />
                     </div>
                 </div>
+                <div 
+                    className="absolute inset-0 md:hidden bg-cover" 
+                    style={{ 
+                        backgroundImage: `url(${home2})`, 
+                        filter: 'blur(2px)',
+                        transform: 'scale(0.8)' 
+                    }}
+                ></div>
             </div>
-            }
-
-        </section>
-  )
-}
+        )}
+    </section>
+    )
+};
